9023b6e16eac9a0e256aa6006f275f734b28b594
chen.fang <chen.fang@le.com>
2016-12-05 16:16:28 -0800

    [add] testNG

--- src/test/java/com/steven/java/AppTest.java
+++ src/test/java/com/steven/java/AppTest.java
@@ -1,27 +1,37 @@
 package com.steven.java;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
 
 /**
- * Unit test for simple App.
+ * Selenium Test with TestNG
  */
-public class AppTest extends TestCase {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName ) { super( testName ); }
+public class AppTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite() { return new TestSuite( AppTest.class ); }
+  private WebDriver driver;
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp() { assertTrue( true ); }
+  @BeforeTest
+  public void beforeTest() {
+    System.setProperty("webdriver.gecko.driver", "src/test/java/com/steven/java/driver/geckodriver");
+    System.setProperty("webdriver.chrome.driver", "src/test/java/com/steven/java/driver/chromedriver");
+    driver = new ChromeDriver();
+    driver.manage().window().maximize();
+  }
+
+  @AfterTest
+  public void afterTest() {
+        driver.quit();
+    }
+
+  @Test
+  public void helloTest() {
+    driver.get("https://chenfang.me");
+    final String title = driver.getTitle();
+    Assert.assertTrue(title.contains("Chen Fang"));
+  }
 }
